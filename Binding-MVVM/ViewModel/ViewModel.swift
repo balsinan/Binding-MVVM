cad18de64ecdfe56d31165a6203f7074a7779f1d
Sinan <balsinan01@hotmail.com>
2021-10-26 12:51:06 +0300

    Add ViewModel

---
+++ Binding-MVVM/ViewModel/ViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ViewModel.swift
+//  Binding-MVVM
+//
+//  Created by Sinan on 26.10.2021.
+//
+
+import Foundation
+
+protocol UserViewModelProtocol {
+    func fetchUser()
+    var users: Observable<[User]> { get  set }
+    
+}
+
+struct UserListViewModel: UserViewModelProtocol{
+    var users: Observable<[User]> = Observable([])
+    var service: UserInfoService = UserInfoService()
+    
+    func fetchUser() {
+        service.fetchUsers { userModelList in
+            users.value = userModelList
+        } onFail: { error in
+            print(error)
+            
+        }
+        
+    }
+    
+}
+ 
